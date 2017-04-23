a297db5d1a695d4323f56f137954feb1cccc40a1
gvozdb <i@owl.kz>
2017-04-24 00:45:43 +0500

    1.1.5-beta

--- _build/data/transport.snippets.php
+++ _build/data/transport.snippets.php
@@ -15,6 +15,10 @@ $tmp = array(
         'file' => 'tvssTags',
         'description' => '',
     ),
+    'tvssCloud' => array(
+        'file' => 'tvssCloud',
+        'description' => '',
+    ),
 );
 
 foreach ($tmp as $k => $v) {
@@ -24,13 +28,13 @@ foreach ($tmp as $k => $v) {
         'id' => 0,
         'name' => $k,
         'description' => @$v['description'],
-        'snippet' => getSnippetContent($sources['source_core'].'/elements/snippets/snippet.'.$v['file'].'.php'),
+        'snippet' => getSnippetContent($sources['source_core'] . '/elements/snippets/snippet.' . $v['file'] . '.php'),
         'static' => BUILD_SNIPPET_STATIC,
         'source' => 1,
-        'static_file' => 'core/components/'.PKG_NAME_LOWER.'/elements/snippets/snippet.'.$v['file'].'.php',
+        'static_file' => 'core/components/' . PKG_NAME_LOWER . '/elements/snippets/snippet.' . $v['file'] . '.php',
     ), '', true, true);
 
-    $properties = include $sources['build'].'properties/properties.'.$v['file'].'.php';
+    $properties = include $sources['build'] . 'properties/properties.' . $v['file'] . '.php';
     $snippet->setProperties($properties);
 
     $snippets[] = $snippet;
